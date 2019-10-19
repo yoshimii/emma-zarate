4fd534422303ef9d3c3351d9f41a22fb297f6ffb
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-19 18:14:37 -0500

    about me links and icons

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,14 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
   },
+  
 });
 
 const About = () => {
@@ -33,17 +36,22 @@ const About = () => {
             About the Developer
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Emma is a full-stack engineer whose curiosity took her around the globe. She found that most of the world's problems can be solved with technology (& a little heart). She has returned home to prove just that. 
+            Emma is a full-stack engineer whose curiosity took her around the globe. She found that most of the world's problems can be solved with technology (& a little heart). She would love to work with you! 
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
+        <Link href="https://twitter.com/emmargherd" target="_blank" rel="noopener" className={classes.link}>
+          <TwitterIcon />
+        </Link>
         </Button>
-        <Button size="small" color="primary">
+        <Link href="https://dev.to/yoshimii" target="_blank" rel="noopener" className={classes.link}>
+        <i class="fab fa-dev fa-2x"></i>
+        </Link>
+        {/* <Button size="small" color="primary">
           Learn More
-        </Button>
+        </Button> */}
       </CardActions>
     </Paper>
   );
