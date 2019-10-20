b1000668bad27bb9438a726ea113e33f925a140a
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-19 20:16:00 -0500

    added Better Reads project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,12 +8,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Proj2 from '../images/deforestation_dashboard_home.png'
+import DeforestationDashBoard from '../images/deforestation_dashboard_home.png';
+import WaitingList from '../images/waiting_list.png';
+import BetterReads from '../images/better_reads.png';
 import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
+    height: 440
   },
   media: {
     height: 140,
@@ -38,7 +41,7 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image={Proj2}
+          image={DeforestationDashBoard}
           title="Contemplative Reptile"
         />
         <CardContent>
@@ -46,7 +49,7 @@ export default function MediaCard() {
             Deforestation Dashboard
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Material UI, React, Node, Charts.js, Python Simple app where the user can display and store deforestation graphs and charts for quick secure access.
+          Material UI, React, Node, Charts.js, Python <br/>Simple app where the user can display and store deforestation graphs and charts for quick secure access.
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -70,26 +73,26 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image={Proj2}
+          image={WaitingList}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Deforestation Dashboard
+            Waiting <br/>List
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Material UI, React, Node, Charts.js, Python Simple app where the user can display and store deforestation graphs and charts for quick secure access.
+          Material UI, React, Reducer Pattern<br/>Host life just got easier. A host can update a party on the fly and keep track of check in time with automatic time stamps.
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
       <Button size="small" color="primary">
-        <Link href="https://github.com/bw-deforestationdashboard" target="_blank" rel="noopener" className={classes.link}>
+        <Link href="https://github.com/yoshimii/reducer-todo" target="_blank" rel="noopener" className={classes.link}>
           Code
         </Link>
         </Button>
         <Button size="small" color="primary">
-        <Link href="https://deforestationdashboard.netlify.com/map-view" target="_blank" rel="noopener" className={classes.link}>
+        <Link href="https://waitinglist.netlify.com/" target="_blank" rel="noopener" className={classes.link}>
           Try It Out
         </Link>
         </Button>
@@ -102,26 +105,26 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image={Proj2}
+          image={BetterReads}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Deforestation Dashboard
+            Better Reads
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Material UI, React, Node, Charts.js, Python Simple app where the user can display and store deforestation graphs and charts for quick secure access.
+          Material UI, React/Redux, Node/Express, Python <br/> A smarter way to search. Uses machine learning and natural language processing to improve search results based on human speach instead of the standard genre/author search.
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
       <Button size="small" color="primary">
-        <Link href="https://github.com/bw-deforestationdashboard" target="_blank" rel="noopener" className={classes.link}>
+        <Link href="https://github.com/better-reads-9-20/Front-End" target="_blank" rel="noopener" className={classes.link}>
           Code
         </Link>
         </Button>
         <Button size="small" color="primary">
-        <Link href="https://deforestationdashboard.netlify.com/map-view" target="_blank" rel="noopener" className={classes.link}>
+        <Link href="https://betterreads.now.sh" target="_blank" rel="noopener" className={classes.link}>
           Try It Out
         </Link>
         </Button>
