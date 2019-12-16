cf245f3210648a5dd267467ccf46f2e1c6867b82
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-12-15 19:18:04 -0600

    Claned up unused variables and components

--- src/components/About.js
+++ src/components/About.js
@@ -1,63 +1,17 @@
 import React from 'react';
-import Waves from '../images/8bitwaves.png';
-import Twitter from '../images/twitter.png'
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import Link from '@material-ui/core/Link';
 
-const useStyles = makeStyles({
-  card: {
-    margin: 'auto',
-    maxWidth: 700,
-    border: 'outset 10px #3f51b5',
-    backgroundColor: '#fffff2',
-    textAlign: 'left'
-  },
-  
-});
 
 const About = () => {
-  const classes = useStyles();
 
   return (
-    <Paper square={false} className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Waves in Chiba"
-          height="200"
-          image={Waves}
-          title="Waves in Chiba"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            About the Developer
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          The fire to become a web developer first ignited during my education career. I discovered the immediacy of being able solve real problems with my own two hands. This unconventional career progression allowed me to leverage my understanding of customer facing needs and complex technical problems to create elegant and practical web apps.          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-        <Link href="https://twitter.com/emmargherd" target="_blank" rel="noopener" className={classes.link}>
-          <img alt="Twitter logo" src={Twitter}/>
-        </Link>
-        </Button>
-        <Link href="https://dev.to/yoshimii" target="_blank" rel="noopener" className={classes.link}>
-        <i className="fab fa-dev fa-2x"></i>
-        </Link>
-        {/* <Button size="small" color="primary">
-          Learn More
-        </Button> */}
-      </CardActions>
-    </Paper>
+    <section>
+      <h1>
+        About the developer
+      </h1>
+      <p>
+      Emma Zarate is captain and commander of modern JavaScript features and is ready to join an agile web team in attacking the enemy: bugs. She works hard towards reaching the highest ranking web performance in the galaxy.
+      </p>
+    </section>
   );
 }
 
